e4d59949ecf5bb6c8a4cbf0f460bf93c2f674e8f
kjw0208 <134485726+kjw0208@users.noreply.github.com>
2025-01-11 03:24:46 +0900

    3day

---
+++ TEST/src/Day7/BOJ10871.java
@@ -0,0 +1,19 @@
+package Day7;
+
+import java.util.Scanner;
+
+public class BOJ10871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        for (int  i=0; i<n; i++){
+            int temp = sc.nextInt();
+            if (temp<x){
+                System.out.print(temp+" ");
+            }
+        }
+
+    }
+}
