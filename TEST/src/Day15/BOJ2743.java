2c3585b86d4097e35f5c3c11c51f136679903489
kjw0208 <134485726+kjw0208@users.noreply.github.com>
2025-01-20 03:13:12 +0900

    Day6

---
+++ TEST/src/Day15/BOJ2743.java
@@ -0,0 +1,13 @@
+package Day15;
+
+import java.util.Scanner;
+
+public class BOJ2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Text = sc.nextLine();
+
+        int Textle = Text.length();
+        System.out.println(Textle);
+    }
+}
