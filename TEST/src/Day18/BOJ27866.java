c496e02830a114610581e3a4140b3a52f3a2c4e2
kjw0208 <134485726+kjw0208@users.noreply.github.com>
2025-01-25 19:50:14 +0900

    Day8

---
+++ TEST/src/Day18/BOJ27866.java
@@ -0,0 +1,13 @@
+package Day18;
+
+import java.util.Scanner;
+
+public class BOJ27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        int I = sc.nextInt();
+
+        System.out.println(S.charAt(I-1));
+    }
+}
