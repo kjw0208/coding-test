6de3ce6c7040fcca9bd9c8af33fd35c526eb78bc
kjw0208 <134485726+kjw0208@users.noreply.github.com>
2025-02-12 03:12:40 +0900

    Day15

---
+++ TEST/src/Day8/BOJ10797.java
@@ -0,0 +1,20 @@
+package Day8;
+
+import java.util.Scanner;
+
+public class BOJ10797 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int b = sc.nextInt();
+            if (a == b) {
+                sum += 1;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
