4ec7e8d2ffa83d49768c1d974a03e69c717bdb13
kjw0208 <134485726+kjw0208@users.noreply.github.com>
2025-02-17 02:18:41 +0900

    Day18

---
+++ TEST/src/Day12/BOJ15439.java
@@ -0,0 +1,12 @@
+package Day12;
+
+import java.util.Scanner;
+
+public class BOJ15439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println(N*(N-1));
+    }
+}
