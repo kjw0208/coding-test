a6ae8e24abe3fcbc08409a121e7418e99021e0e0
kjw0208 <134485726+kjw0208@users.noreply.github.com>
2025-02-17 18:03:05 +0900

    Day19

---
+++ TEST/src/Day14/BOJ11948.java
@@ -0,0 +1,24 @@
+package Day14;
+
+import java.util.Scanner;
+
+public class BOJ11948 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        int E = sc.nextInt();
+        int F = sc.nextInt();
+        int sum = A+B+C+D+E+F;
+
+        int min_a = Math.min(A, Math.min(B, Math.min(C, D)));
+        int min_b = Math.min(E, F);
+
+        System.out.println(sum-min_a-min_b);
+
+
+
+    }
+}
