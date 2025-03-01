fa81821fabc07b8de97f162653d59f5f6bc689d3
kjw0208 <134485726+kjw0208@users.noreply.github.com>
2025-03-01 15:52:35 +0900

    Day29

---
+++ TEST/src/브론즈3/Day13/BOJ2523.java
@@ -0,0 +1,25 @@
+package 브론즈3.Day13;
+
+import java.util.Scanner;
+
+public class BOJ2523 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ TEST/src/브론즈3/Day13/BOJ3053.java
@@ -0,0 +1,12 @@
+package 브론즈3.Day13;
+
+import java.util.Scanner;
+
+public class BOJ3053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double R = sc.nextDouble();
+        System.out.println(Math.PI * R * R);
+        System.out.println(2 * R * R);
+    }
+}
